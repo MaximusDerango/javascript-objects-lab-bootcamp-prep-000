755d957ad51cbec4a945de9cf67dbf3aa256c748
Maxim Dalton <maximcdalton@gmail.com>
2018-05-30 19:39:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {}
 
 var updateObjectWithKeyAndValue = function(object, key, value) {
-  var new_object = Object.assign(object, {})
+  var new_object = Object.assign({}, object)
   new_object[key] = value
   return new_object
 }
